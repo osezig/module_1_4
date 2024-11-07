bf5333a213ce820409916aad011d4097b0d56b59
Sergey_Iagfarov <113824525+osezig@users.noreply.github.com>
2024-11-07 18:38:05 +0500

    Add files via upload

---
+++ module_1_4.py
@@ -0,0 +1,7 @@
+from builtins import PythonFinalizationError
+
+amount_of_HW_completed = 12
+number_of_hours_spent = 1.5
+course_name = 'Python'
+time_for_one_task = (number_of_hours_spent / amount_of_HW_completed)
+print('Курс:', course_name, 'Всего задач: ', amount_of_HW_completed, 'Среднее время выполнения задания: ', time_for_one_task)
